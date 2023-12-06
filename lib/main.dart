ac91263afba49d5c8fb45d9ed02a431583221248
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-12-07 02:10:04 +0900

    feat: locale 및 theme 지정
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:ticats/app/config/app_color.dart';
 
 void main() async {
   // Ensure that the WidgetsBinding has been initialized
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Locale
+  await initializeDateFormatting();
+  Intl.defaultLocale = 'ko_KR';
 
   // Load .env file
   await dotenv.load(fileName: ".env");
@@ -24,6 +32,22 @@ class MainApp extends StatelessWidget {
       useInheritedMediaQuery: true,
       builder: (_, child) {
         return GetMaterialApp(
+          localizationsDelegates: const [
+            GlobalCupertinoLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('ko', 'KR'),
+          ],
+          locale: const Locale('ko', 'KR'),
+          theme: ThemeData(
+            fontFamily: 'SUIT',
+            useMaterial3: true,
+            colorSchemeSeed: AppColor.primaryNormal,
+            appBarTheme: const AppBarTheme(backgroundColor: Colors.white, elevation: 0.0),
+            scaffoldBackgroundColor: Colors.white,
+          ),
           builder: (context, child) {
             return MediaQuery(
               data: MediaQuery.of(context).copyWith(textScaler: const TextScaler.linear(1.0)),

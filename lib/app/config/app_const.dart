af01d04c4297869b2b9e2e07180617b12ac0b5da
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-12-07 01:49:54 +0900

    feat: app config 추가
    - Bindings
    - Color
    - Const
    - TypeFace
    - Routes
    
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

---
+++ lib/app/config/app_const.dart
@@ -0,0 +1,3 @@
+class AppConst {
+  static const String baseUrl = 'https://www.ticats.r-e.kr/api';
+}
---
+++ lib/app/config/app_typeface.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppTypeFace {
+  // XxLarge
+  static TextStyle xxLargeBold = const TextStyle(fontSize: 32, fontWeight: FontWeight.w700);
+  static TextStyle xxLargeMedium = const TextStyle(fontSize: 32, fontWeight: FontWeight.w500);
+
+  // Xlarge
+  static TextStyle xLargeBold = const TextStyle(fontSize: 28, fontWeight: FontWeight.w700);
+  static TextStyle xLargeSemiBold = const TextStyle(fontSize: 28, fontWeight: FontWeight.w600);
+
+  // Large
+  static TextStyle largeMedium = const TextStyle(fontSize: 28, fontWeight: FontWeight.w500);
+  static TextStyle largeSemiBold = const TextStyle(fontSize: 26, fontWeight: FontWeight.w600);
+
+  // Medium
+  static TextStyle mediumBold = const TextStyle(fontSize: 24, fontWeight: FontWeight.w700);
+  static TextStyle mediumMedium = const TextStyle(fontSize: 24, fontWeight: FontWeight.w500);
+
+  // Small
+  static TextStyle smallBold = const TextStyle(fontSize: 20, fontWeight: FontWeight.w700);
+  static TextStyle smallBold_18 = const TextStyle(fontSize: 18, fontWeight: FontWeight.w700);
+  static TextStyle smallSemiBold = const TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
+
+  // XSmall
+  static TextStyle xSmallSemiBold = const TextStyle(fontSize: 16, fontWeight: FontWeight.w600);
+  static TextStyle xSmallMedium = const TextStyle(fontSize: 14, fontWeight: FontWeight.w500);
+  static TextStyle xSmallBold = const TextStyle(fontSize: 12, fontWeight: FontWeight.w700);
+  static TextStyle xSmallRegular = const TextStyle(fontSize: 12, fontWeight: FontWeight.w400);
+}

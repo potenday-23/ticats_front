57a3846d12952f5196f05a5b40839f04221540ef
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-12-07 16:09:09 +0900

    feat: 애플 로그인 기능 추가
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/presentation/login/controller/login_controller.dart
+++ lib/presentation/login/controller/login_controller.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class LoginController extends GetxController {
+  Future<void> loginWithApple() async {
+    AuthorizationCredentialAppleID credential;
+
+    try {
+      credential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+
+      if (kDebugMode) {
+        print(credential);
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('애플 로그인 실패: $e');
+      }
+    }
+  }
+
   Future<void> loginWithKakao() async {
     User user;
 

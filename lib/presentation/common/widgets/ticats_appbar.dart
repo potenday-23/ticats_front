90021f3d0a6a330bf14ec3cbea8421217b6f92e8
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-12-09 15:28:26 +0900

    feat: AppBar 수정
    - Prebuilt AppBar 추가
    - AppBar theme 지정
    
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

---
+++ lib/presentation/common/widgets/ticats_appbar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:ticats/app/config/app_typeface.dart';
+
+class BackAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const BackAppBar({super.key, this.title});
+
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      leadingWidth: 80.w,
+      leading: Row(
+        children: [
+          SizedBox(width: 24.w),
+          GestureDetector(
+            onTap: () => Get.back(),
+            child: SvgPicture.asset('assets/icons/arrow_left.svg', width: 24.w, height: 24.h),
+          ),
+        ],
+      ),
+      title: Text(title ?? "", style: AppTypeFace.smallBold),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(56.w);
+}
+
+class CloseAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CloseAppBar({super.key, this.title});
+
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      actions: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: GestureDetector(onTap: () => Get.back(), child: Icon(Icons.close, size: 24.w)),
+        ),
+      ],
+      title: Text(title ?? "", style: AppTypeFace.smallBold),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(56.w);
+}

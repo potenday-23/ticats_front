aa9638dbcef66f4829f9ef8c49603778e0039ec8
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-12-14 01:05:38 +0900

    feat: 내 티켓 api 추가
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/domain/usecases/ticket_use_cases.dart
+++ lib/domain/usecases/ticket_use_cases.dart
@@ -6,7 +6,25 @@ import '../repositories/ticket_repository.dart';
 class TicketUseCases {
   final TicketRepository _repository = Get.find<TicketRepository>();
 
+  GetMyTicketUseCase get getMyTicketUseCase => Get.put(GetMyTicketUseCase(_repository));
   GetTotalTicketUseCase get getTotalTicketUseCase => Get.put(GetTotalTicketUseCase(_repository));
+}
+
+class GetMyTicketUseCase {
+  final TicketRepository _repository;
+  GetMyTicketUseCase(this._repository);
+
+  Future<List<Ticket>> execute({
+    List<String>? categorys,
+    String? period,
+    String? start,
+    String? end,
+    String? search,
+  }) async {
+    List<Ticket> result = await _repository.getMyTicket();
+
+    return result;
+  }
 }
 
 class GetTotalTicketUseCase {

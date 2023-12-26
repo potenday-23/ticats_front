92c12cb5147434e7cd493291ca8f9a150f0eed9f
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-12-26 14:00:05 +0900

    feat: CategoryAPI 추가
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/data/repositories_impl/mapper/category_mapper.dart
+++ lib/data/repositories_impl/mapper/category_mapper.dart
@@ -1,13 +1,10 @@
 import 'package:auto_mappr_annotation/auto_mappr_annotation.dart';
 import 'package:ticats/data/models/category_model.dart';
-import 'package:ticats/data/models/ticket_model.dart';
 import 'package:ticats/domain/entities/category.dart';
-import 'package:ticats/domain/entities/ticket.dart';
 
-import 'ticket_mapper.auto_mappr.dart';
+import 'category_mapper.auto_mappr.dart';
 
 @AutoMappr([
-  MapType<TicketModel, Ticket>(),
   MapType<CategoryModel, Category>(),
 ])
-class TicketMappr extends $TicketMappr {}
+class CategoryMappr extends $CategoryMappr {}

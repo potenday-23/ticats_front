d457b80ffeaa281bc6baee964ea153470754098c
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-12-28 11:37:28 +0900

    fix: 회월 탈퇴 후 메인 화면으로 이동
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/presentation/my_page/controller/resign_controller.dart
+++ lib/presentation/my_page/controller/resign_controller.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/foundation.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
+import 'package:ticats/app/config/routes/route_path.dart';
+import 'package:ticats/app/service/auth_service.dart';
 import 'package:ticats/domain/usecases/member_use_cases.dart';
 
 class ResignController extends GetxController {
@@ -13,8 +16,14 @@ class ResignController extends GetxController {
   Future<void> postResign() async {
     try {
       await resignMemberUseCase.execute(resignReasonList);
+
+      AuthService.to.logout();
+      Get.offAllNamed(RoutePath.main);
+
+      Fluttertoast.showToast(msg: "회원 탈퇴가 완료되었습니다 ㅜ.ㅜ");
     } catch (e) {
       debugPrint(e.toString());
+      Fluttertoast.showToast(msg: "회원 탈퇴 중 문제가 발생하였습니다.");
     }
   }
 }

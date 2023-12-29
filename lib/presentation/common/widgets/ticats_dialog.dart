413e30769fc17085145fa26a47d218d9fccbf26f
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-12-29 20:10:01 +0900

    feat: 티켓 삭제 다이얼로그 추가
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/presentation/common/widgets/ticats_dialog.dart
+++ lib/presentation/common/widgets/ticats_dialog.dart
@@ -1,10 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:ticats/app/config/app_color.dart';
 import 'package:ticats/app/config/app_typeface.dart';
 import 'package:ticats/app/service/auth_service.dart';
 import 'package:ticats/presentation/main/controller/main_controller.dart';
+import 'package:ticats/presentation/main/controller/ticket_controller.dart';
+
+showDeleteDialog(BuildContext context, int ticketId) async {
+  return await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return Stack(
+        children: [
+          AlertDialog(
+            contentPadding: EdgeInsets.zero,
+            insetPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            scrollable: true,
+            backgroundColor: Colors.white,
+            surfaceTintColor: Colors.white,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14.r)),
+            elevation: 0,
+            content: Padding(
+              padding: EdgeInsets.only(top: 60.w),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: Column(
+                  children: [
+                    Text("티켓을 삭제하시겠어요?\n삭제한 티켓은 복구되지 않습니다.", style: AppTypeFace.xSmall16SemiBold, textAlign: TextAlign.center),
+                    SizedBox(height: 36.w),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: GestureDetector(
+                            behavior: HitTestBehavior.translucent,
+                            onTap: () => Get.back(),
+                            child: Container(
+                              height: 56.w,
+                              decoration: BoxDecoration(
+                                color: AppColor.grayE5,
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(14.r),
+                                ),
+                              ),
+                              child: Center(
+                                child: Text("아니요", style: AppTypeFace.small18SemiBold),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: GestureDetector(
+                            behavior: HitTestBehavior.translucent,
+                            onTap: () async {
+                              await Get.find<TicketController>().deleteTicket(ticketId);
+                              Get.back();
+                            },
+                            child: Container(
+                              height: 56.w,
+                              decoration: BoxDecoration(
+                                color: AppColor.systemPositiveBlue,
+                                borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(14.r),
+                                ),
+                              ),
+                              child: Center(
+                                child: Text("네", style: AppTypeFace.small18SemiBold.copyWith(color: Colors.white)),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            left: 50.w,
+            top: -210.w,
+            child: Align(alignment: Alignment.centerLeft, child: SvgPicture.asset('assets/cats/cat_eyes.svg')),
+          ),
+        ],
+      );
+    },
+  );
+}
 
 showLogoutDialog(BuildContext context) async {
   return await showDialog(

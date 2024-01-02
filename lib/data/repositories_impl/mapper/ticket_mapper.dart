0ec5947e7bbe0acf60a56c4f845e561063c06e4c
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-01-02 16:57:28 +0900

    fix: Build Warning 해결
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/data/repositories_impl/mapper/ticket_mapper.dart
+++ lib/data/repositories_impl/mapper/ticket_mapper.dart
@@ -1,7 +1,9 @@
 import 'package:auto_mappr_annotation/auto_mappr_annotation.dart';
 import 'package:ticats/data/models/category_model.dart';
+import 'package:ticats/data/models/ticats_member_model.dart';
 import 'package:ticats/data/models/ticket_model.dart';
 import 'package:ticats/domain/entities/category.dart';
+import 'package:ticats/domain/entities/ticats_member.dart';
 import 'package:ticats/domain/entities/ticket.dart';
 
 import 'ticket_mapper.auto_mappr.dart';
@@ -9,5 +11,6 @@ import 'ticket_mapper.auto_mappr.dart';
 @AutoMappr([
   MapType<TicketModel, Ticket>(),
   MapType<CategoryModel, Category>(),
+  MapType<MemberModel, Member>(),
 ])
 class TicketMappr extends $TicketMappr {}

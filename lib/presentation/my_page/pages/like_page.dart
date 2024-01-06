15dff77d7f2df77f900bd6b487d79b1b764d6b89
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-01-06 15:04:34 +0900

    feat: 좋아요 한 티켓 없을 경우의 화면 추가
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/presentation/my_page/pages/like_page.dart
+++ lib/presentation/my_page/pages/like_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ticats/app/config/app_color.dart';
+import 'package:ticats/app/config/app_typeface.dart';
 import 'package:ticats/presentation/common/views/ticats_grid_view.dart';
 import 'package:ticats/presentation/common/widgets/ticats_appbar.dart';
 import 'package:ticats/presentation/main/controller/ticket_controller.dart';
@@ -10,8 +12,20 @@ class LikePage extends GetView<TicketController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const BackAppBar(title: "좋아요한 티켓"),
-      body: TicatsGridView(ticketList: controller.likeTicketList),
+      appBar: const BackAppBar(title: "좋아요 한 티켓"),
+      body: Obx(() {
+        if (controller.likeTicketList.isEmpty) {
+          return Center(
+            child: Text(
+              "좋아요 한 티켓이 없어요.\n좋아요 버튼을 누르면\n이곳에서 모아볼 수 있어요!",
+              style: AppTypeFace.small20Bold.copyWith(color: AppColor.grayAE),
+              textAlign: TextAlign.center,
+            ),
+          );
+        }
+
+        return TicatsGridView(ticketList: controller.likeTicketList);
+      }),
     );
   }
 }

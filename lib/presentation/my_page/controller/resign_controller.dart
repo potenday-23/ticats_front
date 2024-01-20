f96ce13072a37d92f5a28e4c491943bc423fb97c
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-01-20 15:34:37 +0900

    refactor: 로그아웃 및 탈퇴시 홈화면으로 이동하게 수정
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/presentation/my_page/controller/resign_controller.dart
+++ lib/presentation/my_page/controller/resign_controller.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
-import 'package:ticats/app/config/routes/route_path.dart';
 import 'package:ticats/app/service/auth_service.dart';
 import 'package:ticats/domain/usecases/member_use_cases.dart';
 import 'package:ticats/presentation/common/widgets/ticats_dialog.dart';
+import 'package:ticats/presentation/home/controller/home_controller.dart';
+import 'package:ticats/presentation/main/controller/main_controller.dart';
 import 'package:ticats/presentation/main/controller/ticket_controller.dart';
 
 class ResignController extends GetxController {
@@ -19,6 +20,10 @@ class ResignController extends GetxController {
       await resignMemberUseCase.execute(resignReasonList);
 
       await AuthService.to.logout();
+
+      Get.find<HomeController>().tabIndex.value = 0;
+      Get.find<MainController>().changePage(0);
+
       await Get.find<TicketController>().getTickets();
 
       Get.back();
